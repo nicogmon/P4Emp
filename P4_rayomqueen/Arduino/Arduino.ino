c23faf224dc8a518d9b2ff29bb565a81463881ca
Nicolas <n.garciam.2021@alumnos.urjc.es>
2023-12-19 11:34:10 +0100

    version mas velocidad en rectas y falta reconteo de lineas perdidas

---
+++ P4_rayomqueen/Arduino/Arduino.ino
@@ -0,0 +1,356 @@
+#include <Thread.h>
+#include <StaticThreadController.h>
+#include <ThreadController.h>
+#include "FastLED.h"
+#include <Arduino_FreeRTOS.h>
+
+
+#define PIN_RBGLED 4
+#define NUM_LEDS 1
+CRGB leds[NUM_LEDS];
+
+
+#define PIN_ITR20001_LEFT   A2
+#define PIN_ITR20001_MIDDLE A1
+#define PIN_ITR20001_RIGHT  A0
+
+#define TURN_RIGHT 1
+#define STRAIGHT 2
+#define TURN_LEFT 3
+
+#define REACTION_FREC 1
+#define ULTRASONIC_FREC 20
+#define PING_FREC 4000
+
+// Enable/Disable motor control.
+//  HIGH: motor control enabled
+//  LOW: motor control disabled
+#define PIN_Motor_STBY 3
+
+// Group A Motors (Right Side)
+// PIN_Motor_AIN_1: Digital output. HIGH: Forward, LOW: Backward
+#define PIN_Motor_AIN_1 7
+// PIN_Motor_PWMA: Analog output [0-255]. It provides speed.
+#define PIN_Motor_PWMA 5
+
+// Group B Motors (Left Side)
+// PIN_Motor_BIN_1: Digital output. HIGH: Forward, LOW: Backward
+#define PIN_Motor_BIN_1 8
+// PIN_Motor_PWMB: Analog output [0-255]. It provides speed.
+#define PIN_Motor_PWMB 6
+
+#define TRIG_PIN 13  
+#define ECHO_PIN 12 
+
+
+long time = 0;
+long dist = 100;
+int obj_counter = 0;
+
+int lineLost = 1;
+
+int last_dir = 0;
+
+int data[3];
+
+unsigned long start_time;
+
+static void ReactionTask(void* pvParameters){
+  TickType_t xLastWakeTime;
+  int status = 0;
+  while(1)
+  {
+    xLastWakeTime = xTaskGetTickCount();
+    
+
+    data[0] = analogRead(PIN_ITR20001_LEFT); 
+    data[1] = analogRead(PIN_ITR20001_MIDDLE); 
+    data[2] = analogRead(PIN_ITR20001_RIGHT);
+
+    for (int i = 0 ; i < 3; i++){
+      if (data[i] > 600){
+        data[i] = 1;
+      }
+      else if (data[i] < 200){
+        data[i] = 0;
+      }else {
+        //tenemos la opciond e considerar que ha visto algo de negro y por si acaso girar
+        //pero esto igual es demasiado redundante o conflictivo con el comparar tambien 
+        //los posibles datos con mas de un uno tipo 110 o 011 
+        //de hecho habria que cabiar y ver que poner en casod e que sea 2 
+        //data[i = 2]
+      }
+    }
+    status = reaction(data[0], data[1], data[2]);
+    if (status == -1){
+      delay(500);
+      exit(0);
+    }
+
+    //Serial.println("reactionTask");
+    xTaskDelayUntil(&xLastWakeTime, REACTION_FREC);
+    
+  }
+}
+
+
+static void DistanceTask(void* pvParameters){
+  TickType_t xLastWakeTime;
+
+  while(1)
+  {
+
+    xLastWakeTime = xTaskGetTickCount();
+    
+
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);  //Enviamos un pulso de 10us
+    digitalWrite(TRIG_PIN, LOW);
+    time = pulseIn(ECHO_PIN, HIGH);
+    dist = time / 59;  // distancia en cm
+    /*if (dist < 15){
+      Mover_Stop();
+      return;
+    }*/
+
+      //Serial.println("reactionTask");
+    xTaskDelayUntil(&xLastWakeTime, ULTRASONIC_FREC);
+      
+  }
+  
+}
+
+/*static void PingTask(void* pvParameters){
+  TickType_t xLastWakeTime;
+  while(1)
+  {
+
+    xLastWakeTime = xTaskGetTickCount();
+    
+    unsigned long pingTime = millis()/1000;
+    Serial.println(4);
+    Serial.println(pingTime);
+    /*char pingStr[20];
+    snprintf(pingStr, sizeof(pingStr), "%lu", pingTime);
+    Serial.println(pingStr);
+    xTaskDelayUntil(&xLastWakeTime, PING_FREC);
+  }
+
+}*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+
+  Serial.begin(9600);
+  pinMode(PIN_ITR20001_LEFT, INPUT);
+  pinMode(PIN_ITR20001_MIDDLE, INPUT);
+  pinMode(PIN_ITR20001_RIGHT, INPUT);
+
+
+  pinMode(PIN_Motor_STBY, OUTPUT);
+  pinMode(PIN_Motor_AIN_1, OUTPUT);
+  pinMode(PIN_Motor_PWMA, OUTPUT);
+  pinMode(PIN_Motor_BIN_1, OUTPUT);
+  pinMode(PIN_Motor_PWMB, OUTPUT);
+  digitalWrite(PIN_Motor_STBY, HIGH);
+  digitalWrite (PIN_Motor_AIN_1, HIGH);
+  digitalWrite (PIN_Motor_BIN_1, HIGH);
+  
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
+  
+  while(1) {
+
+    if (Serial.available()) {
+
+      String start = Serial.readStringUntil('\n');
+      start.trim();
+      int c = start.toInt();
+      
+      if (c == 1){
+
+
+        // Set Red Green to LED
+        FastLED.showColor(Color(0, 255, 0));
+        break;
+      } 
+
+    }
+  }
+    Serial.println(0);
+    start_time = millis();
+
+  
+
+  xTaskCreate(
+    ReactionTask,
+    "ReactionTask",
+    100,
+    NULL,
+    3,
+    NULL
+  );
+
+  xTaskCreate(
+    DistanceTask,
+    "DistanceTask",
+    100,
+    NULL,
+    2,
+    NULL
+  );
+  
+  /*xTaskCreate(
+    PingTask,
+    "PingTask",
+    100,
+    NULL,
+    1,
+    NULL
+  );*/
+  
+
+
+  FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
+  FastLED.setBrightness(20);
+
+}
+
+
+
+uint32_t Color(uint8_t r, uint8_t g, uint8_t b)
+{
+  return (((uint32_t)r << 16) | ((uint32_t)g << 8) | b);
+}
+
+
+int reaction(int dato1, int dato2, int dato3){
+
+  if (dist < 15){
+    Mover_Stop();
+    Serial.println(2);
+    Serial.println( );
+    /*obj_counter++;
+    if (obj_counter > 20){*/
+    Serial.println(1);
+    unsigned long  end_time = millis() - start_time;
+    Serial.println(end_time);
+    return -1;
+    //}
+    
+  }
+  if (lineLost == 0 && (dato1 != 0 || dato2 != 0 || dato3 != 0)){
+    lineLost = 1;
+    Serial.println(6);
+    Serial.println(7);
+  }
+
+  if (dato1 == 1 && dato2 == 0){
+    Pivotar_Izquierda();
+    last_dir = 0;
+    FastLED.showColor(Color(0,255,0));
+    return TURN_RIGHT;
+  }
+  else if (dato1 == 1 && dato2 == 1){
+    Girar_Izquierda();
+    last_dir = 0;
+    FastLED.showColor(Color(0,255,0));
+    return TURN_RIGHT;
+  }
+  else if (dato3 == 1 && dato2 ==0){
+    Pivotar_Derecha();
+    last_dir = 1;
+    FastLED.showColor(Color(0,255,0));
+    return TURN_LEFT;
+  }
+  else if (dato3 == 1 && dato2 ==1){
+    Girar_Derecha();
+    last_dir = 1;
+    FastLED.showColor(Color(0,255,0));
+    return TURN_LEFT;
+  }
+  else if (dato2 == 1){
+    Mover_Adelante();
+    FastLED.showColor(Color(0,255,0));
+    return STRAIGHT;
+  }
+  else if (dato1 == 0 && dato2 == 0 && dato3 == 0) {
+    if(lineLost == 1){
+      Serial.println(3);
+      lineLost = 0;
+      Serial.println(5);
+    }
+    FastLED.showColor(Color(255,0,0));
+    if (last_dir == 0){
+      Arco_Izquierda();
+    } else if (last_dir == 1){
+      Arco_Derecha();
+    } 
+  }
+  return 0;
+    
+}
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+void Mover_Adelante()
+{ 
+  analogWrite (PIN_Motor_PWMA, 150);
+  analogWrite (PIN_Motor_PWMB, 150);
+}
+
+void Arco_Izquierda()
+{
+  analogWrite (PIN_Motor_PWMA, 100);
+  analogWrite (PIN_Motor_PWMB, 0);
+}
+void Girar_Izquierda()
+{
+  analogWrite (PIN_Motor_PWMA, 100);
+  analogWrite (PIN_Motor_PWMB, 0);
+ 
+}
+
+void Girar_Derecha()
+{
+  analogWrite (PIN_Motor_PWMA,0);
+  analogWrite (PIN_Motor_PWMB, 100);
+}
+
+void Arco_Derecha()
+{
+  analogWrite (PIN_Motor_PWMA, 0);
+  analogWrite (PIN_Motor_PWMB, 100);
+}
+
+
+void Pivotar_Izquierda(){
+  analogWrite (PIN_Motor_PWMA, 100);
+  digitalWrite (PIN_Motor_BIN_1, LOW);
+  analogWrite (PIN_Motor_PWMB, 30);
+  digitalWrite (PIN_Motor_BIN_1, HIGH);
+}
+void Pivotar_Derecha(){
+  analogWrite (PIN_Motor_PWMB, 100);
+  digitalWrite (PIN_Motor_AIN_1, LOW);
+  analogWrite (PIN_Motor_PWMA, 30);
+  digitalWrite (PIN_Motor_AIN_1, HIGH);
+ 
+
+}
+
+void Mover_Stop()
+{
+  analogWrite (PIN_Motor_PWMA, 0);
+  
+  analogWrite (PIN_Motor_PWMB, 0);
+}
---
+++ P4_rayomqueen/ESP32/ESP32.ino
@@ -0,0 +1,238 @@
+#include "WiFi.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <Thread.h>
+#include <StaticThreadController.h>
+#include <ThreadController.h>
+
+const char* ssid = "eduroam";
+const char* password = "anitaTNT";
+const char* mqtt_server = "193.147.53.2";
+const char* topic = "/SETR/2023/11/";
+const int mqtt_port = 21883;
+const char* mqttUser = "nicogmon";
+const char* mqttPassword = "urjc2023";
+
+#define EAP_ANONYMOUS_IDENTITY "20220719anonymous@urjc.es" // leave as it is
+#define EAP_IDENTITY "n.garciam.2021@alumnos.urjc.es"    // Use your URJC email
+#define EAP_PASSWORD "Salocin2003$"            // User your URJC password
+#define EAP_USERNAME "n.garciam.2021@alumnos.urjc.es"    // Use your URJC email
+
+
+
+
+#define RXD2 33
+#define TXD2 4
+
+#define START_LAP 0
+#define END_LAP 1
+#define OBSTACLE_DETECTED 2
+#define LINE_LOST 3
+#define PING 4
+#define INIT_LINE_SEARCH 5
+#define STOP_LINE_SEARCH 6
+#define LINE_FOUND 7 
+#define VISIBLE_LINE 8
+
+ThreadController controller = ThreadController(false);
+Thread Thread1 = Thread();
+
+WiFiClient wifiClient;
+//Adafruit_MQTT_Client mqttClient(&wifiClient, mqtt_server, mqtt_port);
+PubSubClient client(wifiClient);
+
+int start_lap = 0;
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_IDENTITY, EAP_USERNAME, EAP_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+}
+
+void callback_ping(){
+  if (start_lap != 0){
+    StaticJsonDocument<200> jsonDoc;
+    jsonDoc["team_name"] = "Dobbyziosos";
+    jsonDoc["id"] = "11";
+    jsonDoc["action"] = "PING";
+    jsonDoc["time"] = millis();
+
+    String jsonString = "";
+    serializeJson(jsonDoc, jsonString);
+    client.publish(topic, (char *)jsonString.c_str());
+  }
+}
+
+
+
+void MQTT_connect() {
+  //int8_t ret;
+  client.setServer(mqtt_server, mqtt_port);
+  while (!client.connected()) {
+    client.connect("ESP32Client", mqttUser, mqttPassword );
+    Serial.println("Intentando conexión MQTT...");
+    delay(5000);
+  }
+  Serial.println("Conectado al servidor MQTT");
+  Serial2.println(1);
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+
+  Thread1.enabled = true;
+  Thread1.setInterval(4000);
+  Thread1.onRun(callback_ping);
+  controller.add(&Thread1);
+
+
+  initWiFi();
+  MQTT_connect();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Publicar el mensaje en el topic especificado
+  controller.run();
+   // Example: Send the JSON payload when a condition is met
+
+  client.loop();
+  //sendJsonPayload();
+  JsonCreate();
+}
+
+void sendJsonPayload() {
+  // Create a JSON document
+  
+  StaticJsonDocument<200> jsonDoc;
+
+  // Fill the JSON document with data
+  jsonDoc["team_name"] = "Dobbyziosos";
+  jsonDoc["id"] = "11";
+  jsonDoc["action"] = "START_LAP";
+
+  // Serialize the JSON document to a string
+  String jsonString = "";
+  serializeJson(jsonDoc, jsonString);
+
+  // Publish the JSON payload to an MQTT topic
+  client.publish(topic, (char *)jsonString.c_str());
+  delay(5000);
+}
+
+void JsonCreate(){//(int action, int value){
+  String value;
+  int action = 0;
+  double int_value = 0;
+  /*if (Serial2.available() > 0) {
+    int action = Serial2.read();
+    Serial.println(action);*/
+
+  if (Serial2.available()) {
+    String receivedString = Serial2.readStringUntil('\n');
+    if (receivedString.length() > 0) {
+      action = receivedString.toInt();
+      Serial.print("Número recibido: ");
+      Serial.println(action);
+
+      if (action == 2 || action == 1 || action == 4 || action == 8){
+        value = Serial2.readStringUntil('\n');
+        int_value = value.toInt();
+        Serial.println(int_value);
+        
+      }
+    }
+
+  /*if (Serial2.available()) {
+    value = Serial2.readStringUntil('\n');
+    if (receivedString.length() > 0) {
+      Serial.println(value);
+    }
+  }*/
+    /*while (Serial2.available() > 0){ // Lee el primer byte (acción)
+      value += Serial2.read(); // Lee el segundo byte (valor asociado a la acción)
+    }*/
+    
+
+    StaticJsonDocument<200> jsonDoc;
+
+    jsonDoc["team_name"] = "Dobbyziosos";
+    jsonDoc["id"] = "11";
+    jsonDoc["action"] = getAction(action);
+    if (action == 0){
+      start_lap = 1;
+    }
+    if (action == 1 || action == 4 ){
+      jsonDoc["time"] = int_value;
+    }
+    if (action == 2){
+      //char StrDistance[16] = "";
+      //sprintf(StrDistance, "%u",value);
+      jsonDoc["distance"] = int_value;
+    }
+    if (action == 8){
+      //char StrValue[16] = "";
+      //sprintf(StrValue, "%u",value);
+      jsonDoc["value"] = int_value;
+    }
+    String jsonString = "";
+    serializeJson(jsonDoc, jsonString);
+    client.publish(topic, (char *)jsonString.c_str());
+     
+    if (action == 1){
+      Thread1.enabled = false;
+    }
+
+  }
+
+}
+
+String getAction(int action){
+  if (action == 0){
+    return "START_LAP";
+  }
+  if (action == 1){
+    return "END_LAP";
+  }
+  if (action == 2){
+    return "OBSTACLE_DETECTED";
+  }
+  if (action == 3){
+    return "LINE_LOST";
+  }
+  if (action == 4){
+    return "PING";
+  }
+  if (action == 5){
+    return "INIT_LINE_SEARCH";
+  }
+  if (action == 6){
+    return "STOP_LINE_SEARCH";
+  }
+  if (action == 7){
+    return "LINE_FOUND";
+  }
+  if (action == 8){
+    return "VISIBLE_LINE";
+  }
+  else{
+    return "ERROR";
+  }
+  
+}
+
